356c4ef749fb56f19f022374408a5643d1cfd915
Naoki Morita <namorit@gmail.com>
2016-01-09 02:29:22 +0900

    Add web-development-with-node-and express

--- web-development-with-node-and-express/meadowlark/meadowlark.js
+++ web-development-with-node-and-express/meadowlark/meadowlark.js
@@ -4,19 +4,44 @@ var app = express();
 var fortune = require('./lib/fortune.js');
 
 var handlebars = require('express-handlebars')
-.create({ defaultLayout: 'main'});
+.create({
+  defaultLayout: 'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 app.get('/', function(req, res) {
   res.render('home');
 });
 
 app.get('/about', function(req, res) {
-  res.render('about', { fortune: fortune.getFortune() });
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: '/qa/tests-about.js'
+  });
+});
+
+app.get('/tours/hood-river', function(req, res) {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+  res.render('tours/request-group-rate');
 });
 
 app.use(function(req, res, next) {
