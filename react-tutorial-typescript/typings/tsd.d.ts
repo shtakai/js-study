994385c8f20b955d0d2fc5de1d40cd69a0443632
Naoki Morita <namorit@gmail.com>
2016-01-10 05:26:59 +0900

    Add superagent and marked tsd

--- react-tutorial-typescript/typings/tsd.d.ts
+++ react-tutorial-typescript/typings/tsd.d.ts
@@ -1,3 +1,6 @@
 
 /// <reference path="react/react.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
+/// <reference path="marked/marked.d.ts" />
